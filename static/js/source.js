52f7d732978b478327a88a52695f9794b5c45293
Alexandra Dobkin <Alexandra Dobkin>
2015-12-20 12:36:07 -0800

    working locally, will wait to check if working on deployed

--- static/js/source.js
+++ static/js/source.js
@@ -66,7 +66,7 @@ $(function() {
     function checkAnswer(data){
 
         // URL
-        var source = $("#source").val();  
+        var source = $("#source").val();
 
         // if the URL is valid
         if(data === "True"){
@@ -78,7 +78,7 @@ $(function() {
 
             // the user is searching for http://textsource.herokuapp.com
             else{
-                // $('#myModal').modal('show');
+                $('#myModal').modal('show');
 
                 // enable search again    
                 $("#submit-btn").removeAttr("disabled");
